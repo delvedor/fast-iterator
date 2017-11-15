6fa7efa9ee536fff76dead36fe010950cd331668
delvedor <tommydelved@gmail.com>
2017-11-15 16:37:26 +0100

    Updated test

--- test.js
+++ test.js
@@ -45,3 +45,59 @@ test('Basic', t => {
     t.deepEqual(value, v4)
   }
 })
+
+test('In case of error should skip to done', t => {
+  t.plan(8)
+
+  const v1 = { hello: 'world' }
+  const v2 = { ciao: 'mondo' }
+  const context = { context: true }
+
+  const fast = Fast([fn1, fn2, fn3], context)
+  t.is(typeof fast, 'function')
+  fast(iterator.bind({ a: 'a', b: 'b' }), v1, done)
+
+  function iterator (fn, value, next) {
+    return fn(this.a, this.b, value, next)
+  }
+
+  function fn1 (a, b, value, done) {
+    t.deepEqual(value, v1)
+    t.deepEqual(this, context)
+    done(null, v2)
+  }
+
+  function fn2 (a, b, value, done) {
+    t.deepEqual(value, v2)
+    t.deepEqual(this, context)
+    done(new Error('kaboom'))
+  }
+
+  function fn3 (a, b, value, done) {
+    t.fail('We should not be here')
+  }
+
+  function done (err, value) {
+    t.is(err.message, 'kaboom')
+    t.deepEqual(this, context)
+    t.deepEqual(value, v2)
+  }
+})
+
+test('Throw on wrong parameters', t => {
+  t.plan(2)
+
+  try {
+    Fast(null)
+    t.fail()
+  } catch (err) {
+    t.is(err.message, 'The first parameter should be an array of functions')
+  }
+
+  try {
+    Fast([null])
+    t.fail()
+  } catch (err) {
+    t.is(err.message, 'The array should contain only functions')
+  }
+})
