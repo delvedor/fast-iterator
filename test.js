6168e559e03dc8aed2ae5f75e462960a1a1e1620
delvedor <tommydelved@gmail.com>
2018-01-05 19:25:42 +0100

    Updated test

--- test.js
+++ test.js
@@ -122,3 +122,93 @@ test('Clean next', t => {
     t.deepEqual(value, v1)
   }
 })
+
+test('Should handle promises', t => {
+  t.plan(10)
+
+  const v1 = { hello: 'world' }
+  const v2 = { ciao: 'mondo' }
+  const v3 = { winter: 'is coming' }
+  const v4 = { winter: 'has come' }
+  const context = { context: true }
+
+  const fast = Fast([fn1, fn2, fn3], context)
+  t.is(typeof fast, 'function')
+  fast(iterator.bind({ a: 'a', b: 'b' }), v1, done)
+
+  function iterator (fn, value, next) {
+    return fn(this.a, this.b, value, next)
+  }
+
+  function fn1 (a, b, value) {
+    t.deepEqual(value, v1)
+    t.deepEqual(this, context)
+    return new Promise((resolve, reject) => {
+      resolve(v2)
+    })
+  }
+
+  function fn2 (a, b, value, done) {
+    t.deepEqual(value, v2)
+    t.deepEqual(this, context)
+    return new Promise((resolve, reject) => {
+      resolve(v3)
+    })
+  }
+
+  function fn3 (a, b, value, done) {
+    t.deepEqual(value, v3)
+    t.deepEqual(this, context)
+    return new Promise((resolve, reject) => {
+      resolve(v4)
+    })
+  }
+
+  function done (err, value) {
+    t.error(err)
+    t.deepEqual(this, context)
+    t.deepEqual(value, v4)
+  }
+})
+
+test('In case of error should skip to done (with promises)', t => {
+  t.plan(8)
+
+  const v1 = { hello: 'world' }
+  const v2 = { ciao: 'mondo' }
+  const context = { context: true }
+
+  const fast = Fast([fn1, fn2, fn3], context)
+  t.is(typeof fast, 'function')
+  fast(iterator.bind({ a: 'a', b: 'b' }), v1, done)
+
+  function iterator (fn, value, next) {
+    return fn(this.a, this.b, value, next)
+  }
+
+  function fn1 (a, b, value, done) {
+    t.deepEqual(value, v1)
+    t.deepEqual(this, context)
+    return new Promise((resolve, reject) => {
+      resolve(v2)
+    })
+  }
+
+  function fn2 (a, b, value, done) {
+    t.deepEqual(value, v2)
+    t.deepEqual(this, context)
+    return new Promise((resolve, reject) => {
+      reject(new Error('kaboom'))
+    })
+  }
+
+  function fn3 (a, b, value, done) {
+    t.fail('We should not be here')
+  }
+
+  function done (err, value) {
+    t.is(err.message, 'kaboom')
+    t.deepEqual(this, context)
+    t.deepEqual(value, v2)
+  }
+})
